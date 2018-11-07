2f3aae193f725a13273773d5c382de3655566c1a
Woody Lucas <wlucas0311@gmail.com>
2018-11-07 03:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
