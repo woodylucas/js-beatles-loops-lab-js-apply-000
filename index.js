5f080ddb7075d9343e5d676e52bdbde455699ac7
Woody Lucas <wlucas0311@gmail.com>
2018-11-07 03:26:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(array) {
   var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice" ];
   var lennonFacts = [];
+  var i = 0
   while() {
     
   }
